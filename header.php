a87a00569ff4ee86e6dc20d355e36dfa5e45656f
aalbert.bach2023 <albertaxel2110@gmail.com>
2022-04-13 11:55:06 +0200

    début level 2

--- header.php
+++ header.php
@@ -9,5 +9,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/f0a61fdca5.js" crossorigin="anonymous"></script>
+    <style>
+        .bordure td{
+            border:1px solid black;
+            padding:3px;
+            box-sizing: border-box;
+        }
+        .petitTableau td {
+            width:13px;
+            height:13px;
+        }
+        .petitTableau td.vert{
+            background-color: green;
+        }
+    </style>
 </head>
 <body>
