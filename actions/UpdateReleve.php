62cd7eae1d4ab12653ab2f659df47d1fdbd4ee4d
hmorisset.bach2023 <hmorisset85@gmail.com>
2022-04-13 13:54:33 +0200

    add config.php / add creerListe.php / UpdateReleve.php and config.php

--- actions/UpdateReleve.php
+++ actions/UpdateReleve.php
@@ -7,19 +7,19 @@ if ($_SESSION["token"] != filter_input(INPUT_POST, "token")) {
 else
     $_SESSION["token"]=uniqid();
 
-$dateReleve = filter_input(INPUT_POST, "dateReleve");
-$lieu = filter_input(INPUT_POST, "lieu");
-$releve = filter_input(INPUT_POST, "releve");
+$id = filter_input(INPUT_POST, "id");
+$dateReleve = filter_input(INPUT_POST, "DateReleve");
+$lieu = filter_input(INPUT_POST, "Lieu");
+$releve = filter_input(INPUT_POST, "Releve");
 
 include_once "../config.php";
 $pdo = new PDO("mysql:host=".Config::SERVEUR.";dbname=".Config::BDD, Config::UTILISATEUR, Config::MOTDEPASSE);
-$req=$pdo->prepare("insert into releve (DateReleve, Lieu, Releve) values (:Date, :Lieu, :Releve)");
+$req=$pdo->prepare("update releve set  DateReleve=:Date, Lieu=:Lieu, Releve=:Releve,  where id=:id");
+$req->bindParam(":id", $id);
 $req->bindParam(":DateReleve", $Date);
 $req->bindParam(":Lieu", $Lieu);
 $req->bindParam(":Releve", $Releve);
 
 $req->execute();
 
-$id=$pdo->lastInsertId();
-
-header("location: ../index.php?id=$id");
+header("location: ../index.php");
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+class Config{
+    const SERVEUR = "localhost";
+    const BDD = "esaip";
+    const UTILISATEUR = "root";
+    const MOTDEPASSE = "";
+}
