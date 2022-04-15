2884b7cb2be024772bacceb518a72c15b18496e8
Mat <pmatteo032002@gmail.com>
2022-04-15 16:37:17 +0200

    fin level 3

--- header.php
+++ header.php
@@ -10,6 +10,15 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/f0a61fdca5.js" crossorigin="anonymous"></script>
     <style>
+        table
+        {
+            border-spacing : 0;
+            border-collapse : collapse;
+        }
+        table td{
+            border-width: 0;
+            padding:0;
+        }
         .bordure td{
             border:1px solid black;
             padding:3px;
